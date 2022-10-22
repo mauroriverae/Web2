38dfcaeb79342c789346780c16c0b6cc5e80365e
Mauro Rivera <mauororiverae@gmail.com>
2022-10-22 15:00:49 -0300

    agregando seguridad

--- Seguridad/EjemploUno/registro.php
+++ Seguridad/EjemploUno/registro.php
@@ -3,8 +3,12 @@
         echo '
         <h2>Registro</h2>
         <form  method="post">
-            <input name= "email" type="email" placeholder ="ingrese su email">
+            <input name= "email"  placeholder ="ingrese su email">
             <input  name= "password" type="password"  placeholder ="ingrese su contraseña">
+            <select name="select">
+                <option value="adm">adm</option>
+                <option value="invited">invited</option>
+            </select>
             <button>Crear cuenta</button>
         </form>
         ';
@@ -15,10 +19,11 @@
 
     if(!empty($_POST['email'])&& !empty($_POST['password'])){
         $userEmail= $_POST['email'];
+        $userLevel = $_POST['select'];
         $userPassword = password_hash($_POST['password'], PASSWORD_BCRYPT); 
         
         $db = new PDO('mysql:host=localhost;'.'dbname=hashing;charset=utf8','root','');
-        $query= $db->prepare('INSERT INTO users(email, password)VALUES(?, ?)');
-        $query->execute([$userEmail, $userPassword]);
+        $query= $db->prepare('INSERT INTO users(email, level, password)VALUES(?, ?, ?)');
+        $query->execute([$userEmail, $userLevel, $userPassword]);
       
     }
